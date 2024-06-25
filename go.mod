f19e457435b47cbb6397d8fb95841e9c7fa84343
jcbadeoye <159209104+jcbadeoye@users.noreply.github.com>
2024-06-25 12:23:16 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jcbadeoye/booking-app
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("welcome to our conference booking-application")
+	fmt.Println("Get your tickets here to attend")
+}
